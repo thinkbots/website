4c12fb636ff9744add400a53750af2b83a818838
Raoul DIFFOUO <diraulo@gmail.com>
2018-04-03 02:27:57 +0200

    Add site SEO meta

---
+++ src/config/index.js
@@ -0,0 +1,13 @@
+module.exports = {
+  description:
+    'Thinkbots is a  Web development agency, specializing in building Ruby on Rails apps. We also build android applications.',
+  keywords:
+    'Ruby on Rails, Agile Software Development, Android app development, Digital Design, Craft Academy SA, Web-development Training',
+
+  site: 'Thinkbots',
+  title: 'Web and mobile apps development and design | Thinkbots',
+
+  twitter: '@thinkbotsIO',
+  twitter_title: 'Thinkbots - Web and mobile apps development and design',
+  url: 'https://thinkbots.io',
+}
--- src/layouts/index.js
+++ src/layouts/index.js
@@ -5,17 +5,52 @@ import Helmet from 'react-helmet'
 import Header from '../components/Header'
 import Footer from '../components/Footer'
 
+import config from '../config'
+
+import image from '../images/logo_md.png'
+import favicon from '../images/favicon.ico'
+
 import '../stylesheets/all.scss'
+
+const schemaOrgJSONLD = [
+  {
+    '@context': 'http://schema.org',
+    '@type': 'WebSite',
+    url: config.url,
+    name: config.site,
+    alternateName: config.title,
+  },
+]
 
 const TemplateWrapper = ({ children }) => (
   <div>
-    <Helmet
-      title="Web and mobile apps development and design | Thinkbots"
-      meta={[
-        { name: 'description', content: 'Sample' },
-        { name: 'keywords', content: 'sample, something' },
-      ]}
-    />
+    <Helmet defaultTitle={config.title} titleTemplate={`%s | ${config.site}`}>
+      <link rel="shortcut icon" href={favicon} />
+      <meta name="description" content={config.description} />
+      <meta name="image" content={image} />
+
+      {/* Schema.org tags */}
+      <script type="application/ld+json">
+        {JSON.stringify(schemaOrgJSONLD)}
+      </script>
+
+      {/* OpenGraph tags */}
+      <meta property="og:url" content={config.url} />
+      <meta name="og:site_name" content={config.site} />
+      <meta name="og:type" content="website" />
+      <meta name="og:title" content={config.title} />
+      <meta name="og:description" content={config.description} />
+      <meta name="og:image" content={image} />
+
+      {/* Twitter Card tags */}
+      <meta name="twitter:site" content={config.twitter} />
+      <meta name="twitter:card" content={config.twitter_title} />
+      <meta name="twitter:creator" content={config.twitter} />
+      <meta name="twitter:title" content={config.twitter_title} />
+      <meta name="twitter:description" content={config.description} />
+      <meta name="twitter:image" content={image} />
+    </Helmet>
+
     <Header />
     <div>{children()}</div>
     <Footer />
